6c605e69dc39b66c77598910fdb9e6cf46fccca1
xiaohuli <waterdoor@foxmail.com>
2020-02-05 20:58:08 +0800

    fix: 后台内容

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ app.post(apiPrefix + '/login', async function(req,res){
             type,
             currentAuthority: 'user',
             //  用户请求的鉴权token
-            accessToken: jwt.encode(Object.assign({ tokenTimeStamp: Date.now() }, req.body), secret)
+            accessToken: jwt.encode(Object.assign(req.body, { tokenTimeStamp: Date.now() } ), secret)
         });
     } else {
         res.send({
@@ -117,52 +117,14 @@ app.post(apiPrefix + '/login', async function(req,res){
 });
 const aa = {
     name: 'wang',
+    showName: '',
     avatar: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
     userid: '00000001',
     email: 'antdesign@alipay.com',
     signature: '海纳百川，有容乃大',
     title: '交互专家',
     group: '蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED',
-    tags: [
-      {
-        key: '0',
-        label: '很有想法的',
-      },
-      {
-        key: '1',
-        label: '专注设计',
-      },
-      {
-        key: '2',
-        label: '辣~',
-      },
-      {
-        key: '3',
-        label: '大长腿',
-      },
-      {
-        key: '4',
-        label: '川妹子',
-      },
-      {
-        key: '5',
-        label: '海纳百川',
-      },
-    ],
-    notifyCount: 12,
-    unreadCount: 11,
     country: 'China',
-    geographic: {
-      province: {
-        label: '浙江省',
-        key: '330000',
-      },
-      city: {
-        label: '杭州市',
-        key: '330100',
-      },
-    },
-    address: '西湖区工专路 77 号',
     phone: '0752-268888888',
 };
 
@@ -234,12 +196,12 @@ app.post(apiPrefix + '/currentUser', async function(req,res){
     const wxToken = await getToken();
     const doamin = 'https://api.weixin.qq.com/tcb/databasequery?access_token=' + wxToken;
     if (verifyToken(req.body)) {
-        // let a = await ownTool.netModel.post(doamin, {
-        //     //access_token: token,
-        //     env: 'test-psy-qktuk',
-        //     query: 'db.collection(\"user\").where({name:"' + userName + '"}).get()'
-        // })
-        res.send(aa);
+        let a = await ownTool.netModel.post(doamin, {
+            //access_token: token,
+            env: 'test-psy-qktuk',
+            query: 'db.collection(\"userDetail\").where({name:"' + req.body.name + '"}).get()'
+        })
+        res.send(a);
     } else {
         errorSend(res);
     }
