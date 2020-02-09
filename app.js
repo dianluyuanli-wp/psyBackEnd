b4f0d09cb283218f80ba67537e769bd5c0e45427
xiaohuli <waterdoor@foxmail.com>
2020-02-09 15:58:00 +0800

    fix: 图片上传实现

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var bodyParser = require('body-parser');
 let mongoose = require('mongoose');
 let ownTool = require('xiaohuli-package');
 let { Animal, BookInfo } = require('./modle');
-const { Upload } = require('./postImg');
+//const { Upload } = require('./postImg');
 let fs = require('fs');
+const request = require('request-promise');
 
 var jwt = require('jwt-simple');
 const secret = 'xiaohuli';
@@ -202,55 +203,59 @@ app.post(apiPrefix + '/updateAvatar', async function(req,res){
     const wxToken = await getToken();
     const doamin = 'https://api.weixin.qq.com/tcb/uploadfile?access_token=' + wxToken;
     if (verifyToken(req.body)) {
-        const { token, base64 : originDataUrl, ...rest } = req.body;
+        const { token, name, base64 : originDataUrl, ...rest } = req.body;
         var base64 = originDataUrl.replace(/^data:image\/\w+;base64,/, "");//去掉图片base64码前面部分data:image/png;base64
         var dataBuffer = new Buffer(base64, 'base64'); //把base64码转成buffer对象，
-        fs.writeFile('temp.png',dataBuffer,function(err){//用fs写入文件
+        const imgName = name + '.png';
+        //  把前端传过来的base64转化成本地图片
+        fs.writeFile(imgName, dataBuffer,function(err){//用fs写入文件
             if(err){
                 console.log(err);
             }else{
-                console.log('写入成功！');
+                //  console.log('写入成功！');
             }
         })
-        //  删除掉老文件
-        // fs.readdirSync('../chat/dist/avatar').map((item) => {
-        //     if (eval('/' + i_am + '_avatar_/ig').exec(item)) {
-        //         fs.unlink('../chat/dist/avatar/' + item, (e) => {
-        //             if(e) {
-        //                 console.log(e);
-        //             }
-        //         })
-        //     }
-        // })
-
+        //  获取图片上传相关信息
         let a = await ownTool.netModel.post(doamin, {
             env: 'test-psy-qktuk',
-            path: 'temp.png'
-            //  query: fs.createReadStream('temp.png')
+            path: imgName
         })
-        const { authorization, url, token: newToken, cos_file_id} = a;
-        console.log(a, fs.createReadStream('temp.png'));
-        // var reader = new FileReader();
-        // let buffer;
-        // reader.onload = function(e) {
-        //     buffer = reader.result;
-        // }
-        // reader.readAsBinaryString(file);
-        //  ownTool.netModel.post await Upload
-
-        let b = await Upload(url, {
-            //env: 'test-psy-qktuk',
-            key: 'temp.png',
-            Signature: authorization,
-            'x-cos-security-token': newToken,
-            'x-cos-meta-fileid': cos_file_id,
-            file: {
-                value: fs.createReadStream('temp.png')
+        const { authorization, url, token: newToken, cos_file_id, file_id} = a;
+        //  真正上传图片
+        const option = {
+            method: 'POST',
+            uri: url,
+            formData: {
+                "Signature": authorization,
+                "key": imgName,
+                "x-cos-security-token": newToken,
+                "x-cos-meta-fileid": cos_file_id,
+                "file": {
+                    value: fs.createReadStream(imgName),
+                    options: {
+                        filename: 'test',
+                        //contentType: file.type
+                    }
+                }
             }
-            //  query: fs.createReadStream('temp.png')
+        }
+        await request(option);
+        //  获取图片的下载链接
+        const getDownDomain = 'https://api.weixin.qq.com/tcb/batchdownloadfile?access_token=' + wxToken;
+        let imgInfo = await ownTool.netModel.post(getDownDomain, {
+            env: 'test-psy-qktuk',
+            file_list: [{
+                fileid: file_id,
+                max_age: 7200
+            }]
+        });
+        //  server中转的图片删掉
+        fs.unlink(imgName, (e) => {
+            if(e) {
+                console.log(e);
+            }
         })
-        console.log(b);
-        res.send(b);
+        res.send(imgInfo);
     } else {
         errorSend(res);
     }
